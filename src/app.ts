708431b477b72f7a7c185349668c21bbf7766c73
SnorlaxSPI <aflinsjr@gmail.com>
2021-09-12 14:42:46 -0300

    pt 3 - done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata';
-import express from 'express';
+import './config/env';
+import 'express-async-errors';
+import express, { Request, Response, NextFunction } from 'express';
 import './database/connect';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -10,6 +13,13 @@ app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀🚀 Server Started');
+});
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ status: 'error', message: err.message });
+  }
+  return response.status(500).json({ status: 'error', message: 'Internal server error' });
 });
 
 export default { app };
